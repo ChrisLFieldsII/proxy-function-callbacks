7b1b515d9a0d0306563d6bea2c69fba28cae6895
Chris Fields II <chrisjr06@gmail.com>
2021-12-12 16:57:53 -0600

    test: + case for sync error function

--- src/get-proxy.test.ts
+++ src/get-proxy.test.ts
@@ -3,19 +3,25 @@ import sinon from 'sinon'
 
 import { getFnCbProxy } from './get-proxy'
 
+const ERROR_TAG = 'ERROR'
+
 class NameManager {
   names: string[] = []
 
   addName = (name: string) => {
     this.names.push(name)
   }
+
+  throwError = () => {
+    throw new Error(ERROR_TAG)
+  }
 }
 
 let nameManager = new NameManager()
-let onBefore = sinon.spy()
-let onSuccess = sinon.spy()
-let onError = sinon.spy()
-let onAfter = sinon.spy()
+const onBefore = sinon.spy()
+const onSuccess = sinon.spy()
+const onError = sinon.spy()
+const onAfter = sinon.spy()
 let proxy = getFnCbProxy({
   target: nameManager,
   onBefore,
@@ -25,11 +31,9 @@ let proxy = getFnCbProxy({
 })
 
 test.beforeEach(() => {
+  const spies = [onBefore, onSuccess, onError, onAfter]
+  spies.forEach((spy) => spy.resetHistory())
   nameManager = new NameManager()
-  onBefore = sinon.spy()
-  onSuccess = sinon.spy()
-  onError = sinon.spy()
-  onAfter = sinon.spy()
   proxy = getFnCbProxy({
     target: nameManager,
     onBefore,
@@ -39,11 +43,7 @@ test.beforeEach(() => {
   })
 })
 
-test.afterEach(() => {
-  sinon.restore()
-})
-
-test('sync - success - callbacks called in order', (t) => {
+test.serial('sync - success - callbacks called in order', (t) => {
   proxy.addName('cri')
 
   t.true(onBefore.calledOnce)
@@ -53,6 +53,14 @@ test('sync - success - callbacks called in order', (t) => {
   sinon.assert.callOrder(onBefore, onSuccess, onAfter)
 })
 
-// test('sync - error - callbacks called in order', () => {
+test.serial('sync - error - callbacks called in order', (t) => {
+  t.throws(() => proxy.throwError(), {
+    message: ERROR_TAG,
+  })
 
-// })
+  t.true(onBefore.calledOnce, 'onBefore')
+  t.true(onSuccess.notCalled, 'onSuccess')
+  t.true(onError.calledOnce, 'onError')
+  t.true(onAfter.calledOnce, 'onAfter')
+  sinon.assert.callOrder(onBefore, onError, onAfter)
+})
