41c725e4e5ac6a1b112a1e9d382fd643c35f3ce9
Chris Fields II <chrisjr06@gmail.com>
2021-12-12 19:40:18 -0600

    test: + cases for async functions

--- src/get-proxy.test.ts
+++ src/get-proxy.test.ts
@@ -1,4 +1,5 @@
 import test from 'ava'
+import isPromise from 'is-promise'
 import sinon from 'sinon'
 
 import { getFnCbProxy } from './get-proxy'
@@ -12,8 +13,25 @@ class NameManager {
     this.names.push(name)
   }
 
+  addNameAsync = (name: string) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        this.addName(name)
+        resolve()
+      }, 1500)
+    })
+  }
+
   throwError = () => {
     throw new Error(ERROR_TAG)
+  }
+
+  throwAsyncError = () => {
+    return new Promise<void>((_, reject) => {
+      setTimeout(() => {
+        reject(new Error(ERROR_TAG))
+      }, 1500)
+    })
   }
 }
 
@@ -74,6 +92,30 @@ test.serial('sync - success - callbacks called in order', (t) => {
 
 test.serial('sync - error - callbacks called in order', (t) => {
   t.throws(() => proxy.throwError(), {
+    message: ERROR_TAG,
+  })
+
+  t.true(onBefore.calledOnce, 'onBefore')
+  t.true(onSuccess.notCalled, 'onSuccess')
+  t.true(onError.calledOnce, 'onError')
+  t.true(onAfter.calledOnce, 'onAfter')
+  sinon.assert.callOrder(onBefore, onError, onAfter)
+})
+
+test.serial('async - success - callbacks called in order', async (t) => {
+  const res = proxy.addNameAsync('cri')
+  t.true(isPromise(res))
+  await res
+
+  t.true(onBefore.calledOnce)
+  t.true(onSuccess.calledOnce)
+  t.true(onAfter.calledOnce)
+  t.true(onError.notCalled)
+  sinon.assert.callOrder(onBefore, onSuccess, onAfter)
+})
+
+test.serial('async - error - callbacks called in order', async (t) => {
+  await t.throwsAsync(() => proxy.throwAsyncError(), {
     message: ERROR_TAG,
   })
 
