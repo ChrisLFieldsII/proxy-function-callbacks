43d583008f4398f26ee7ed8e765e69232a48894a
Chris Fields II <chrisjr06@gmail.com>
2021-12-19 20:33:38 -0600

    add a nextjs example using proxy

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,24 +1,158 @@
+import { FormEvent, useMemo, useState } from 'react'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import {
   Container,
   Heading,
   Text,
   List,
   ListItem,
-  ListIcon,
-  OrderedList,
-  UnorderedList,
+  Input,
+  HStack,
+  Button,
+  Box,
+  Checkbox,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatGroup,
+  IconButton,
+  ButtonGroup,
+  useToast,
 } from '@chakra-ui/react'
-import { TodoManager } from '../src/todo'
+import { getFnCbProxy } from 'proxy-function-callbacks'
+import { DeleteIcon } from '@chakra-ui/icons'
+import { TodoData, TodoManager, formTodoData } from '../src/todo'
 
 const todoManager = new TodoManager()
 
 const Home: NextPage = () => {
+  const [text, setText] = useState<string>('')
+  const [todoData, setTodoData] = useState<TodoData>(formTodoData([]))
+  const toast = useToast()
+
+  const proxy = useMemo(() => {
+    return getFnCbProxy({
+      target: todoManager,
+      onSuccess(cmd) {
+        console.log('success proxy:', { cmd })
+
+        const todoData = cmd.data as TodoData
+        setTodoData(todoData)
+      },
+      onBefore(cmd) {
+        console.log('before proxy:', { cmd })
+      },
+      onAfter(cmd) {
+        console.log('after proxy:', { cmd })
+      },
+      onError(cmd) {
+        console.error('error proxy:', { cmd })
+
+        const error = cmd.error as Error
+        toast({
+          status: 'error',
+          description: error.message,
+          title: error.name,
+        })
+      },
+    })
+  }, [toast])
+
+  const clearText = () => setText('')
+
+  const onSubmit = (e: FormEvent<HTMLDivElement>) => {
+    e.preventDefault()
+
+    console.log('adding todo: ', text)
+
+    try {
+      proxy.addTodo(text)
+      clearText()
+    } catch (error) {
+      // delegate all error handling to `onError` callback
+    }
+  }
+
   return (
     <Container maxW='container.lg'>
       <Heading textAlign='center'>Todos - Proxy Function Callbacks</Heading>
+
+      <Box my='12' />
+
+      <StatGroup>
+        <Stat>
+          <StatLabel># Todos</StatLabel>
+          <StatNumber>{todoData.numTodos}</StatNumber>
+        </Stat>
+
+        <Stat>
+          <StatLabel># Complete Todos</StatLabel>
+          <StatNumber>{todoData.numCompleteTodos}</StatNumber>
+        </Stat>
+
+        <Stat>
+          <StatLabel># Incomplete Todos</StatLabel>
+          <StatNumber>{todoData.numIncompleteTodos}</StatNumber>
+        </Stat>
+      </StatGroup>
+
+      <Box my='6' />
+
+      <HStack as='form' onSubmit={onSubmit}>
+        <Input
+          value={text}
+          placeholder='Enter todo'
+          onChange={(e) => setText(e.currentTarget.value)}
+        />
+        <Button type='submit'>Add</Button>
+      </HStack>
+
+      <Box my='3' />
+
+      <ButtonGroup>
+        <Button onClick={proxy.clearTodos}>Clear Todos</Button>
+
+        <Button onClick={() => proxy.markAllTodosComplete(true)}>
+          Mark All Todos Complete
+        </Button>
+
+        <Button onClick={() => proxy.markAllTodosComplete(false)}>
+          Mark All Todos Incomplete
+        </Button>
+      </ButtonGroup>
+
+      <Box my='12' />
+
+      <List spacing={3}>
+        {todoData.todos.map((todo) => {
+          return (
+            <ListItem
+              key={todo.id}
+              display={'flex'}
+              flexDir={'row'}
+              alignItems={'center'}
+            >
+              <Checkbox
+                isChecked={todo.isComplete}
+                onChange={(e) =>
+                  proxy.markTodoComplete(todo.id, e.currentTarget.checked)
+                }
+              />
+              <Box mx='1' />
+              <IconButton
+                size={'sm'}
+                colorScheme={'red'}
+                variant={'ghost'}
+                icon={<DeleteIcon />}
+                aria-label='delete todo'
+                onClick={() => proxy.removeTodo(todo.id)}
+              />
+              <Box mx='3' />
+              <Text>{todo.text}</Text>
+            </ListItem>
+          )
+        })}
+      </List>
     </Container>
   )
 }
--- example/src/todo.ts
+++ example/src/todo.ts
@@ -6,7 +6,7 @@ interface Todo {
   isComplete: boolean
 }
 
-interface TodoData {
+export interface TodoData {
   todos: Todo[]
   numTodos: number
   numCompleteTodos: number
@@ -15,33 +15,67 @@ interface TodoData {
 
 interface ITodoManager {
   getTodoData: () => TodoData
-  addTodo: (input: Omit<Todo, 'id'>) => TodoData
+  addTodo: (text: string) => TodoData
   removeTodo: (id: string) => TodoData
   clearTodos: () => TodoData
+  markTodoComplete: (id: string, isComplete: boolean) => TodoData
+  markAllTodosComplete: (isComplete: boolean) => TodoData
 }
 
 export class TodoManager implements ITodoManager {
   todos: Todo[] = []
 
-  getTodoData = () => _getTodoData(this.todos)
+  getTodoData = () => formTodoData(this.todos)
 
-  addTodo = (input: Omit<Todo, 'id'>) => {
-    this.todos = this.todos.concat({ ...input, id: cuid() })
-    return _getTodoData(this.todos)
+  addTodo = (text: string) => {
+    if (!text) {
+      throw new Error('Must enter a todo')
+    }
+
+    this.todos = this.todos.concat({ id: cuid(), text, isComplete: false })
+    return formTodoData(this.todos)
   }
 
   removeTodo = (id: string) => {
     this.todos = this.todos.filter((todo) => todo.id !== id)
-    return _getTodoData(this.todos)
+    return formTodoData(this.todos)
   }
 
   clearTodos = () => {
     this.todos = []
-    return _getTodoData(this.todos)
+    return formTodoData(this.todos)
+  }
+
+  markTodoComplete = (id: string, isComplete: boolean) => {
+    const todo = this.findTodo(id)
+
+    todo.isComplete = isComplete
+
+    this.todos = [...this.todos]
+
+    return formTodoData(this.todos)
+  }
+
+  markAllTodosComplete = (isComplete: boolean) => {
+    this.todos = this.todos.map((todo) => ({ ...todo, isComplete }))
+    return formTodoData(this.todos)
+  }
+
+  private findTodo = (id: string) => {
+    const todo = this.todos.find((todo) => todo.id === id)
+
+    if (!todo) {
+      throw new Error('Failed to find todo')
+    }
+
+    return todo
   }
 }
 
-function _getTodoData(todos: Todo[]): TodoData {
+/**
+ * @desc Build the form data shape given an array of Todos
+ */
+export function formTodoData(todos: Todo[]): TodoData {
   const numTodos = todos.length
   const numCompleteTodos = todos.filter((todo) => todo.isComplete).length
 
